dc6ce1623dd976cdfbfde2ebeb0dda6be7b182e5
Ginokun <meghinasso@gmail.com>
2021-04-22 14:03:35 -0300

    Se comentaron las funciones y variables.

--- src/lock_peripherials.c
+++ src/lock_peripherials.c
@@ -12,16 +12,16 @@
 
 /*==================[definiciones y macros]==================================*/
 
-#define KEYPAD_ROW_SIZE 4
-#define KEYPAD_COL_SIZE 4
-#define KEYBOARD_DELAY	5000		//Tiempo de lectura de teclado en ms.
-#define GREEN_LED_TIME	4000		//Tiempo de aviso de acceso valido en ms.
-#define RED_LED_TIME	1500		//Tiempo de aviso acceso invalido en ms.
-#define	ACCEPTED_BUZZER_PERIOD	1
-#define	REJECTED_BUZZER_PERIOD	8
+#define KEYPAD_ROW_SIZE 4			/**<Numero de filas que tiene el teclado matricial*/
+#define KEYPAD_COL_SIZE 4			/**<Numero de columnas que tiene el teclado matricial*/
+#define KEYBOARD_DELAY	5000		/**<Tiempo de lectura de teclado en ms.*/
+#define GREEN_LED_TIME	4000		/**<Tiempo de aviso de acceso valido en ms.*/
+#define RED_LED_TIME	1500		/**<Tiempo de aviso acceso invalido en ms.*/
+#define	ACCEPTED_BUZZER_PERIOD	1	/**<Periodo de la frecuencia (en ms) del buzzer al haberse ingresado un código válido*/
+#define	REJECTED_BUZZER_PERIOD	8	/**<Periodo de la frecuencia (en ms) del buzzer al haberse ingresado un código inválido*/
 
-#define PASSWORD_INIT	"******************************"
-#define ADMIN_PASSWORD	"1234"
+#define PASSWORD_INIT	"******************************"	/**<Datos para inicializar el string donde se van a guardar las contraseñas temporales ingresadas*/
+#define ADMIN_PASSWORD	"1234"		/**<Contraseña del administrador (no debe ser mayor a MAX_PASSWORD*/
 
 /*==================[definiciones de datos internos]=========================*/
 
@@ -53,7 +53,7 @@ bool_t turnOffBuzzer(gpioMap_t buzzer);
  */
 bool_t keypadToDigit(uint16_t key, char * tempDigit) {
 	static const char keypad[] = { '1', '2', '3', '\0', '4', '5', '6', '\0',
-			'7', '8', '9', '\0', '\0', '0', '\0', '\0' }; //Valores de los botones del teclado.
+			'7', '8', '9', '\0', '\0', '0', '\0', '\0' }; 						/**<Valores de los botones del teclado*/
 
 	if (key >= 0 && key <= sizeof(keypad)) {
 		*tempDigit = (char) keypad[key];
@@ -63,61 +63,77 @@ bool_t keypadToDigit(uint16_t key, char * tempDigit) {
 }
 
 /**
- * @brief 
+ * @brief Enciende un led.
  * 
- * @param led 
- * @return bool_t 
+ * @param led Salida donde se encuentra conectado el led.
+ * @return bool_t TRUE si logro completarse la tarea. FALSE caso contrario.
  */
 bool_t turnOnLed(gpioMap_t led) {
 	return gpioWrite(led, ON);
 }
 
 /**
- * @brief 
+ * @brief Apaga un led.
  * 
- * @param led 
- * @return bool_t 
+ * @param led Salida donde se encuentra conectado el led.
+ * @return bool_t TRUE si logro completarse la tarea. FALSE caso contrario.
  */
 bool_t turnOffLed(gpioMap_t led) {
 	return gpioWrite(led, OFF);
 }
 
+/**
+ * @brief Enciende un buzzer.
+ * 
+ * @param buzzer Salida donde se encuentra conectado el buzzer.
+ * @return bool_t TRUE si logro completarse la tarea. FALSE caso contrario.
+ */
 bool_t turnOnBuzzer(gpioMap_t buzzer) {
 	return gpioWrite(buzzer, ON);
 }
 
+/**
+ * @brief Apaga un buzzer.
+ * 
+ * @param buzzer Salida donde se encuentra conectado el buzzer.
+ * @return bool_t TRUE si logro completarse la tarea. FALSE caso contrario.
+ */
 bool_t turnOffBuzzer(gpioMap_t buzzer) {
 	return gpioWrite(buzzer, OFF);
 }
+
+/**
+ * @brief Inicializa el pin donde se encuentra conectado el buzzer como salida.
+ * 
+ */
 void buzzerInit() {
 	gpioInit(GPIO8, GPIO_OUTPUT);
 }
 
 /*==================[definiciones de funciones externas]=====================*/
 
-/********************************************************
- /*************************userInit**********************
- /********************************************************
- *
- *
- ********************************************************/
-
+/**
+ * @brief Inicializa las variables internas de la estructura user.
+ * 
+ * @param user Puntero a una estructura tipo user_t.
+ */
 void userInit(user_t * user) {
 	user->accessAutho = FALSE;
 	strcpy(user->password, ADMIN_PASSWORD);
 }
 
-/********************************************************
- /**************UpdateMatrixKeyboard**********************
- /********************************************************
- *
- *
- ********************************************************/
-
+/**
+ * @brief Revisa si se presiono algun botón del teclado matricial. 
+ * En caso de haberse presionado, se almacena su valor en la variable tempPassword dentro de admin.
+ * 
+ * @param admin Estructura donde adentro tiene una variable para almacenar la contraseña temporal ingresada.
+ * @return bool_t TRUE si se presiono alguna tecla (hubo un intento de ingreso). FALSE caso contrario.
+ * 
+ */
 bool_t UpdateMatrixKeyboard(user_t * admin) {
 
-	bool_t button_flag = FALSE;	//button_flag indica si la tecla se solto o sigue presionada.
-	uint16_t tempKey; //Variable para almacenar el valor temporal de la tecla presionada.
+	bool_t button_flag = FALSE;					/**<Indica si la tecla se solto o sigue presionada.*/
+	uint16_t tempKey; 							/**<Variable para almacenar el valor temporal de la tecla presionada.*/
 	uint8_t i = 0;
 	delay_t keyboardDelay;
 	bool_t retVal = FALSE;
@@ -125,6 +141,7 @@ bool_t UpdateMatrixKeyboard(user_t * admin) {
 
 	delayInit(&keyboardDelay, KEYBOARD_DELAY);
 	do {
+		/*Lectura del teclado matricial*/
 		if (keypadRead(&matrixPad, &tempKey) && (button_flag == FALSE)) {
 
 			delayInit(&keyboardDelay, KEYBOARD_DELAY);
@@ -140,6 +157,7 @@ bool_t UpdateMatrixKeyboard(user_t * admin) {
 		} else if (!keypadRead(&matrixPad, &tempKey) && (button_flag == TRUE)) {
 			button_flag = FALSE;
 		}
+		/********************************/
 
 		/*Condiciones de salida del loop while*/
 		if (i >= MAX_PASSWORD) {//La contraseña ingresada supero el largo valido de la contraseña.
@@ -156,18 +174,16 @@ bool_t UpdateMatrixKeyboard(user_t * admin) {
 			stdioPrintf(UART_USB, "Limite de tiempo = %s \n", tempPassword);
 			return retVal;
 		}
+		/***************************************/
 	} while (retVal == TRUE);
-	return retVal;
 
+	return retVal;
 }
 
-/********************************************************
- /********************lockInit****************************
- /********************************************************
- *
- *
- ********************************************************/
-
+/**
+ * @brief Inicializa la UART, el teclado matricial  y el buzzer.
+ * 
+ */
 void lockInit() {
 
 	uartConfig(UART_USB, 115200);
@@ -176,6 +192,12 @@ void lockInit() {
 	buzzerInit();
 }
 
+/**
+ * @brief Notifica al usuario si se ingreso una contraseña válida. 
+ * La notificación consta del encendido de un led verde y el buzzer con una frecuencia aguda.
+ * 
+ * @return bool_t 
+ */
 bool_t ValidPass() {
 	delay_t delayGreenLed;
 	delay_t delayAcceptedBuzzer;
@@ -195,6 +217,12 @@ bool_t ValidPass() {
 	return TRUE;
 }
 
+/**
+ * @brief Notifica al usuario si se ingreso una contraseña inválida. 
+ * La notificación consta del encendido de un led rojo y el buzzer con una frecuencia grave.
+ * 
+ * @return bool_t 
+ */
 bool_t InvalidPass() {
 	delay_t delayRedLed;
 	delay_t delayRejectedBuzzer;
@@ -215,6 +243,13 @@ bool_t InvalidPass() {
 	return TRUE;
 }
 
+/**
+ * @brief Comparpa la contraseña ingresada con la contraseña almacenada del uusario. 
+ * 
+ * @param admin 	Estructura con la información del uusario.
+ * @return bool_t 	TRUE si la contraseña ingresada es igual a la almacenada.
+ *					FALSE si la contraseña ingresada no es correcta.
+ */
 bool_t ValidatePass(user_t * admin) {
 	if (!strcmp(admin->password, admin->tempPassword)) {
 		return TRUE;
@@ -223,6 +258,14 @@ bool_t ValidatePass(user_t * admin) {
 	}
 }
 
+/**
+ * @brief 	Verifica si hay datos nuevos ingresados por la UART. 
+ * En caso de haberlos, los lee y copia en la contraseña temporal dentro del usuario.
+ * 
+ * @param admin 	Estructura con la información del uusario.
+ * @return bool_t 	TRUE si se presiono alguna tecla (hubo un intento de ingreso). 
+ * 					FALSE caso contrario.
+ */
 bool_t UpdateUart(user_t * admin) {
 	char tempPassword[] = PASSWORD_INIT;
 	uint8_t i;
@@ -244,6 +287,11 @@ bool_t UpdateUart(user_t * admin) {
 
 }
 
+/**
+ * @brief Notifica al usuario de que ocurrio un error.
+ * 
+ * @return bool_t 
+ */
 bool_t Error (){
 	delay_t delayYellowLed;
 	delay_t delayRejectedBuzzer;
